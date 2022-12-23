f5d570d6dddf79de4c24b3e489745d3bdc17aaa2
djcaron <djcaron@gmail.com>
2022-12-23 18:52:31 -0500

    TerritoryIQ

---
+++ assets/playbar/playbarScript.js
@@ -0,0 +1,51 @@
+cp.playbarAssetArr = 
+[
+	'AudioOff',
+	'AudioOn',
+	'BackGround',
+	'Backward',
+	'Color',
+	'ColorSmall',
+	'CC',
+	'Exit',
+	'FastForward',
+	'FastForward1',
+	'FastForward2',
+	'Forward',
+	'Glow',
+	'GlowSmall',
+	'Height',
+	'Play',
+	'Pause',
+	'Progress',
+	'Rewind',
+	'Thumb',
+	'ThumbBase',
+	'TOC'
+];
+cp.playbarTooltips = 
+{
+	AudioOff : 'Audio Off',
+	AudioOn : 'Audio On',
+	BackGround : 'BackGround',
+	Backward : 'Backward',
+	Color : 'Color',
+	CC : 'CC',
+	Exit : 'Exit',
+	FastForward : '2x Fast Forward Speed',
+	FastForward1 : '4x Fast Forward Speed',
+	FastForward2 : 'Normal Speed',
+	Forward : 'Forward',
+	Glow : 'Glow',
+	Height : 'Height',
+	Play : 'Play',
+	Pause : 'Pause',
+	Progress : 'Progress',
+	Rewind : 'Rewind',
+	Thumb : 'Thumb',
+	ThumbBase : 'ThumbBase',
+	TOC : 'TOC'
+};
+cp.handleSpecialForPlaybar = function(playbarConstruct)
+{
+}
